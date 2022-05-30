1cf7260646a46be605ea2057873588ea175a7830
vincentmckelvie <78393715+vincentmckelvie@users.noreply.github.com>
2022-05-29 19:12:21 -0700

    added alpha support

--- frame-sequence.py
+++ frame-sequence.py
@@ -3,12 +3,12 @@ import bmesh
 
 scene = bpy.context.scene
 src = bpy.data.objects['Plane']
-frames = 100
+frames = 99
 col = bpy.data.collections.new(name="OUTPUT")
 bpy.context.scene.collection.children.link(col)
 imgName = "frames_" 
 zeroPads = 4
-for x in range(1,frames+1):
+for x in range(0,frames+1):
     
     newObj = src.copy()
     newObj.data = src.data.copy()
@@ -27,9 +27,12 @@ for x in range(1,frames+1):
     texImage.image = bpy.data.images.load( bpy.path.abspath("//"+imgName+str(x).zfill(4)+".png") )
     
     box_material_obj.node_tree.links.new(bsdf.inputs['Base Color'], texImage.outputs['Color'])
+    box_material_obj.node_tree.links.new(bsdf.inputs['Alpha'], texImage.outputs['Alpha'])
+    box_material_obj.blend_method = 'BLEND'
     #box_material_obj.node_tree.links.new(bsdf.inputs['Emissive'], texImage.outputs['Color'])
     newObj.data.materials.append(box_material_obj)
     
+
     bpy.context.scene.frame_set(x)
     
     newObj.scale = [0.001,0.001,0.001]
